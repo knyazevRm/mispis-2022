c3276f11a0ff61aec4c3ce406095c45f82fc8f5c
knyazevRm <gasmenas@gmail.com>
2022-12-10 20:08:29 +0300

    first commit

---
+++ Lab 10/mispis/Faculty.java
@@ -0,0 +1,24 @@
+public class Faculty {
+
+	public Faculty(Employee dean, String name) {
+		this.dean = dean;
+		this.name = name;
+	}
+
+	public Employee dean;
+	public String name;
+
+	public void day_open_door(){
+
+	}
+	public void employTeachers(){
+
+	}
+	public void sackTeacher(){
+
+	}
+	public void makeInquiry(){
+
+	}
+
+}
---
+++ Lab 10/mispis/Institute.java
@@ -0,0 +1,23 @@
+public class Institute {
+
+	public Institute(String name, String address) {
+		this.name = name;
+		this.address = address;
+	}
+
+	public String name;
+	public String address;
+
+	public void time_open(){
+
+	}
+	public void time_close(){
+
+	}
+
+	public Integer showRating(){
+		Integer rating = 100;
+		return rating;
+	}
+
+}
